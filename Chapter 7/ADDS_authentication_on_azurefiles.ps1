83f3bc37a4867d249402a281cd12e67c39450acf
Don <Don>
2023-12-17 00:10:05 +0200

    add content for ADDS Authentication script

--- Chapter 7/ADDS_authentication_on_azurefiles.ps1
+++ Chapter 7/ADDS_authentication_on_azurefiles.ps1
@@ -1,1 +1,3 @@
-# ARM template for VM connecting to AD VM
+Import-Module -name AZFilesHybrid;
+Connect-AzAccount;
+Join-AzStorageAccountForAuth -ResourceGroupName "AZ104-StorageAccounts" -StorageAccountName "somefileshare16122023" -Domain "storagedemo.com" -OrganizationalUnitName "OU=AzureShares,OU=Az104_Resources,DC=domainname,DC=com"
