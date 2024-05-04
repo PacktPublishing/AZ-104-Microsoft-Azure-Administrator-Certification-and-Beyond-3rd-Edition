e8b6d6b4396b5ebeb3e17a49219afb88234aaea1
Don <Don>
2024-05-04 11:43:50 +0200

    Chapter20

--- Chapter 20/Creating_an_Applications_Insights_Resources.ps1
+++ Chapter 20/Creating_an_Applications_Insights_Resources.ps1
@@ -2,7 +2,6 @@
 $ResourceGroup = "AZ104-Monitor"
 $Location = "WestEurope"
 $AppServicePlanName = "mylinuxappserviceplan10101"
-$gitrepo="https://github.com/ColorlibHQ/AdminLTE.git"
 $webappname="mywebapp$(Get-Random)"
 
 # Create an App Service Plan for Linux
@@ -10,15 +9,6 @@ New-AzAppServicePlan -Name $AppServicePlanName -Tier Free -Location $Location -L
 
 # Create a Web App
 New-AzWebApp -Name $WebAppName -ResourceGroupName $ResourceGroup -Location $Location -AppServicePlan $AppServicePlanName
-Publish-AzWebApp -Name $WebAppName -ResourceGroupName $ResourceGroup -ArchivePath ""
 
-# Modify the Web App to Dotnet
-Set-AzWebApp -Name $WebAppName -ResourceGroupName $ResourceGroup -AppServicePlan $AppServicePlanName -NetFrameworkVersion 6
-
-# Configure GitHub deployment from your GitHub repo and deploy once.
-$PropertiesObject = @{
-    repoUrl = "$gitrepo";
-    branch = "master";
-    isManualIntegration = "true";
-}
-Set-AzResource -Properties $PropertiesObject -ResourceGroupName $ResourceGroup -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $WebAppName/web -ApiVersion 2015-08-01 -Force
+# Publish the Web Files to the Website
+Publish-AzWebApp -Name $WebAppName -ResourceGroupName $ResourceGroup -ArchivePath "C:\webfiles\testwebsite.zip"
